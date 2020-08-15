ad5126d82e097b7ad6bb3997ced40119d28a1373
Kevin Chau <kevin@kevinchau.com>
2020-08-14 18:48:39 -0700

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Kevin Chau on 8/14/20.
+//  Copyright © 2020 Kevin Chau. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
